f067ac5247100f696c0aee29bf0a18bef370a48e
simonxue <xueyao.me@gmail.com>
2022-06-24 23:09:05 +0800

    feat:启动应用添加部分提示日志

--- src/main/java/me/xueyao/JeabFastBlogApiApplication.java
+++ src/main/java/me/xueyao/JeabFastBlogApiApplication.java
@@ -1,18 +1,34 @@
 package me.xueyao;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author simonxue
  */
+@Slf4j
 @SpringBootApplication
 @MapperScan("me.xueyao.mapper")
 public class JeabFastBlogApiApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JeabFastBlogApiApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(JeabFastBlogApiApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application Jeab Fast Blog is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port  + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + "/\n\t" +
+                "Swagger文档: \thttp://" + ip + ":" + port  + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 
 }
